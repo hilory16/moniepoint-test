b7068e8c8c072c5dc5995bdf5a069938b119811d
Emmanuel  Ilori <olamidembp@Emmanuels-MacBook-Pro.local>
2023-04-03 01:42:23 +0100

    Carousel banner ease settings

--- src/components/AnimatedRoutes/index.js
+++ src/components/AnimatedRoutes/index.js
@@ -1,17 +1,17 @@
-import { Routes, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-// import Home from "pages/Home";
-// import Product from "pages/Product";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { AnimatedPresence } from "framer-motion";
+import Home from "pages/Home";
+import Product from "pages/Product";
 
 export default function AnimatedRoutes() {
   const location = useLocation();
 
   return (
-    <AnimatePresence>
+    <AnimatedPresence>
       <Routes location={location} key={location.pathname}>
-        {/* <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<Product />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
       </Routes>
-    </AnimatePresence>
+    </AnimatedPresence>
   );
 }
