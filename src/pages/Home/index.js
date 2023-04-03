9e6c8d15bafc263923c4a7f4da195cd98ecf5870
Emmanuel  Ilori <olamidembp@Emmanuels-MacBook-Pro.local>
2023-04-03 01:50:54 +0100

    Carousel banner ease settings

---
+++ src/pages/Home/index.js
@@ -0,0 +1,25 @@
+import { motion } from "framer-motion";
+import Banner from "components/Banner";
+import Header from "components/Header";
+import { AppWrapper } from "components/Wrappers";
+import QuickActions from "components/QuickActions";
+import ProductList from "components/ProductList";
+import BottomNav from "components/BottomNav";
+
+export default function Home() {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0, transition: { duration: 0.3 } }}
+    >
+      <Header />
+      <Banner />
+      <AppWrapper>
+        <QuickActions />
+      </AppWrapper>
+      <ProductList />
+      <BottomNav />
+    </motion.div>
+  );
+}
