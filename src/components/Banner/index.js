929a7d8921662f1cd33da058b6fd9027f1bab180
Emmanuel  Ilori <olamidembp@Emmanuels-MacBook-Pro.local>
2023-04-03 11:13:15 +0100

    Carousel chrome glitch fix test

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -13,7 +13,7 @@ export default function Banner() {
     autoplay: true,
     speed: 1500,
     autoplaySpeed: 2500,
-    cssEase: "ease-out",
+    cssEase: "linear",
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
